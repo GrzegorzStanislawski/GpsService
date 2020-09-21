1a51917785d51f8d8601f77bfbee7745199dbd9f
Grzegorz Stanisławski <grzegorz.stanislawski@witchcraftstudios.com>
2020-09-21 10:49:06 +0200

    fix: zmiana get na post

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ $router->get('swagger/schema', 'SwaggerController@schema');
 $router->group(['prefix' => 'api'], function () use ($router) {
 	$router->post('address', 'GpsAddressController@address');
 	$router->post('position', 'GpsPositionController@position');
-	$router->post('providers', 'GpsController@providers');
+	$router->get('providers', 'GpsController@providers');
 });
